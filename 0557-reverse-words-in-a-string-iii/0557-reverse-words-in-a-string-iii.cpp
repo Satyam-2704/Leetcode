ca6cb6a3def3389a521cf041356579807c7817b7
Satyam-2704 <sinhasatyam.2704@gmail.com>
2026-05-27 14:16:14 +0530

    Time: 0 ms (100%), Space: 15.2 MB (31%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string word;
+        string result;
+        while(ss >> word){
+            reverse(word.begin(),word.end());
+            if(!result.empty()){
+                result+=" ";
+            }
+            result+=word;
+        }
+        return result;
+    }
+};
