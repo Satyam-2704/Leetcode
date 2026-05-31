1612b82e5a479b3f0452e5f996776233abd54967
Satyam-2704 <sinhasatyam.2704@gmail.com>
2026-05-31 21:39:47 +0530

    Time: 1 ms (19.7%), Space: 9.2 MB (76.51%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int n=t.size();
+        int n=t.size()-1;
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
         for(int i=0;i<t.length();i++){
