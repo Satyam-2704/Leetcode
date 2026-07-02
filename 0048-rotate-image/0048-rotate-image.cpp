30254328eaaf1324915caa594324a183a93af5af
Satyam-2704 <sinhasatyam.2704@gmail.com>
2026-07-02 22:16:25 +0530

    Time: 0 ms (100%), Space: 10.3 MB (34.78%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -9,12 +9,5 @@ public:
     for(int k=0;k<mat.size();k++){
         reverse(mat[k].begin(),mat[k].end());
     }
-    for(int i=0;i<mat.size();i++){
-        for(int j=0;j<mat[0].size();j++){
-            cout<<mat[i][j]<<" ";
-        }
-        cout<<endl;   
-    }
-    
     }
 };
